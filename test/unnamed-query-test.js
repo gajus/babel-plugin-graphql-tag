99c2ebeb7d405b17ffcfe6e7b138745e4c050679
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-09-08 07:31:12 -0400

    Add test for failure case of unnamed queries

---
+++ test/unnamed-query-test.js
@@ -0,0 +1,29 @@
+import { transform } from 'babel-core';
+import assert from 'assert';
+
+const fixture = "gql`type Widget { name: String } query {widget}`";
+
+describe("When given an unnamed query", () => {
+  let originalError;
+
+  before(function() {
+    originalError = console.error;
+  });
+
+  after(function() {
+    console.error = originalError;
+  });
+
+  it('fails when there are other definitions', () => {
+    const calls = [];
+
+    console.error = (...args) => calls.push(args.join(' '));
+
+    transform(fixture, {
+      plugins: [['./src']]
+    });
+
+    assert.equal(calls.length, 1);
+    assert.equal(calls[0], 'error Error: GraphQL query must have name.');
+  });
+});
