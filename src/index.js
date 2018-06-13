528c175d357366eeca17bf89f0f91aff8fd970fa
Rizki Romadhoni <rizki.romadhoni@kumparan.com>
2018-06-13 07:57:05 +0700

    attach documentId to body

--- src/index.js
+++ src/index.js
@@ -55,13 +55,6 @@ export default () => {
     debug('compiling a GraphQL query', source);
 
     const queryDocument = gql(source);
-    if (opts && opts.generateHash) {
-      // generate graphql documentId
-      const hash = crypto.createHash('sha256');
-      hash.update(Buffer.from(print(queryDocument)));
-      // $FlowFixMe inject documentId
-      queryDocument.documentId = hash.digest('base64');
-    }
 
     // If a document contains only one operation, that operation may be unnamed:
     // https://facebook.github.io/graphql/#sec-Language.Query-Document
@@ -74,6 +67,13 @@ export default () => {
     }
 
     const body = parseLiteral(queryDocument);
+    if (opts && opts.generateHash) {
+      // generate graphql documentId
+      const hash = crypto.createHash('sha256');
+      hash.update(Buffer.from(print(queryDocument)));
+      // $FlowFixMe inject documentId
+      body.documentId = hash.digest('base64');
+    }
     let uniqueUsed = false;
 
     if (expressions.length) {
